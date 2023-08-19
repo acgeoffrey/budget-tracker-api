d19f68e0ca05ae07423d97dd1571d07392b5a124
Geoffrey <acgeoffrey@users.noreply.github.com>
2023-08-19 15:24:28 +0530

    add: brevo email service for production

--- utils/email.js
+++ utils/email.js
@@ -1,17 +1,30 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  let transporter;
+
+  if (process.env.NODE_ENV === 'development') {
+    transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.BREVO_HOST,
+      port: process.env.BREVO_PORT,
+      auth: {
+        user: process.env.BREVO_USERNAME,
+        pass: process.env.BREVO_PASSWORD,
+      },
+    });
+  }
 
   const mailOptions = {
-    from: 'Budget Tracker <hello@budgetTracker.com>',
+    from: 'CoinChef | Budget Tracker <noreply@coinchef.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
